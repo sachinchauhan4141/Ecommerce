dc109287357e6e7dbb479438188caa0ac45f2de3
sachinchauhan4141 <Chauhansahab85@gmail.com>
2023-07-14 14:56:33 +0530

    header - footer - completed

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import {Navbar ,Nav,Container, NavbarBrand} from 'react-bootstrap'
+import {FaShoppingCart,FaUser} from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href='/'>
+                    Ecommerce
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse>
+                    <Nav className='ms-auto'>
+                        <Nav.Link href='/cart'>
+                            <FaShoppingCart/>Cart
+                        </Nav.Link>
+                        <Nav.Link href='/cart'>
+                            <FaUser/>Sign-in
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header;
