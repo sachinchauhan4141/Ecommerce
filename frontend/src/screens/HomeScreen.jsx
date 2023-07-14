b44d0b12f13d7b55a8f3e0138101b1d2bdeebb3d
sachinchauhan4141 <Chauhansahab85@gmail.com>
2023-07-14 15:24:19 +0530

    product are added

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+  return <>
+    <h1>Latest Products</h1>
+    <Row>
+        {products.map((p)=>{
+            return <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={p}></Product>
+            </Col>
+        })}
+    </Row>
+  </>;
+};
+
+export default HomeScreen;
